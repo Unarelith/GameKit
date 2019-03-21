70b91f558f6bb81cbcfa8a6e901f1a54662dd8d5
Quentin Bazin <quent42340@gmail.com>
2019-03-21 17:22:15 +0100

    [Rect] Partially documented.

--- include/gk/core/Rect.hpp
+++ include/gk/core/Rect.hpp
@@ -20,35 +20,81 @@
 
 namespace gk {
 
+////////////////////////////////////////////////////////////
+/// \brief Utility class for manipulating 2D axis aligned rectangles
+///
+////////////////////////////////////////////////////////////
 template<typename T>
 class Rect {
 	public:
+		////////////////////////////////////////////////////////////
+		/// \brief Default constructor
+		///
+		/// Creates an empty rectangle (it is equivalent to calling
+		/// Rect(0, 0, 0, 0)).
+		///
+		////////////////////////////////////////////////////////////
 		Rect() = default;
 
+		////////////////////////////////////////////////////////////
+		/// \brief Construct the rectangle from its coordinates
+		///
+		/// Be careful, the last two parameters are the width
+		/// and height, not the right and bottom coordinates!
+		///
+		/// \param _x      Left coordinate of the rectangle
+		/// \param _y      Top coordinate of the rectangle
+		/// \param _width  Width of the rectangle
+		/// \param _height Height of the rectangle
+		///
+		////////////////////////////////////////////////////////////
 		Rect(T _x, T _y, T _width, T _height) {
 			reset(_x, _y, _width, _height);
 		}
 
+		////////////////////////////////////////////////////////////
+		/// \brief Construct the rectangle from position and size
+		///
+		/// Be careful, the last parameter is the size,
+		/// not the bottom-right corner!
+		///
+		/// \param _position Position of the top-left corner of the rectangle
+		/// \param _size     Size of the rectangle
+		///
+		////////////////////////////////////////////////////////////
 		Rect(const Vector2<T> &_position, const Vector2<T> &_size) {
 			reset(_position.x, _position.y, _size.x, _size.y);
 		}
 
+		////////////////////////////////////////////////////////////
+		/// \brief Construct the rectangle from another type of rectangle
+		///
+		/// This constructor doesn't replace the copy constructor,
+		/// it's called only when U != T.
+		/// A call to this constructor will fail to compile if U
+		/// is not convertible to T.
+		///
+		/// \param rect Rectangle to convert
+		///
+		////////////////////////////////////////////////////////////
 		template<typename U>
-		Rect(const Rect<U> &rect)
+		explicit Rect(const Rect<U> &rect)
 			: Rect(rect.x, rect.y, rect.width, rect.height) {}
 
-		void reset(T _x, T _y, T _width, T _height) {
-			x = _x;
-			y = _y;
-			width = _width;
-			height = _height;
-		}
-
-		void reset(Rect<T> rect) { reset(rect.x, rect.y, rect.width, rect.height); }
-
-		void move(T _x, T _y) { x += _x; y += _y; }
-		void move(Vector2<T> d) { move(d.x, d.y); }
-
+		////////////////////////////////////////////////////////////
+		/// \brief Check if a point is inside the rectangle's area
+		///
+		/// This check is non-inclusive. If the point lies on the
+		/// edge of the rectangle, this function will return false.
+		///
+		/// \param _x X coordinate of the point to test
+		/// \param _y Y coordinate of the point to test
+		///
+		/// \return True if the point is inside, false otherwise
+		///
+		/// \see intersects
+		///
+		////////////////////////////////////////////////////////////
 		bool contains(T _x, T _y) const {
 			T minX = std::min(x, static_cast<T>(x + width));
 			T maxX = std::max(x, static_cast<T>(x + width));
@@ -58,10 +104,33 @@ class Rect {
 			return (_x >= minX) && (_x < maxX) && (_y >= minY) && (_y < maxY);
 		}
 
+		////////////////////////////////////////////////////////////
+		/// \brief Check if a point is inside the rectangle's area
+		///
+		/// This check is non-inclusive. If the point lies on the
+		/// edge of the rectangle, this function will return false.
+		///
+		/// \param point Point to test
+		///
+		/// \return True if the point is inside, false otherwise
+		///
+		/// \see intersects, intersectionDirection
+		///
+		////////////////////////////////////////////////////////////
 		bool contains(const Vector2<T> &point) const {
 			return contains(point.x, point.y);
 		}
 
+		////////////////////////////////////////////////////////////
+		/// \brief Check the intersection between two rectangles
+		///
+		/// \param rect Rectangle to test
+		///
+		/// \return True if rectangles overlap, false otherwise
+		///
+		/// \see contains, intersectionDirection
+		///
+		////////////////////////////////////////////////////////////
 		bool intersects(const Rect<T> &rect) const {
 			T r1MinX = std::min(x, static_cast<T>(x + width));
 			T r1MaxX = std::max(x, static_cast<T>(x + width));
@@ -81,6 +150,16 @@ class Rect {
 			return interLeft < interRight && interTop < interBottom;
 		}
 
+		////////////////////////////////////////////////////////////
+		/// \brief Check the intersection direction between two rectangles
+		///
+		/// \param rect Rectangle to test
+		///
+		/// \return 1 for horizontal, 2 for vertical, 0 for no intersection
+		///
+		/// \see contains, intersects
+		///
+		////////////////////////////////////////////////////////////
 		T intersectionDirection(const Rect<T> &rect) const {
 			T r1MinX = std::min(x, static_cast<T>(x + width));
 			T r1MaxX = std::max(x, static_cast<T>(x + width));
@@ -108,28 +187,129 @@ class Rect {
 			}
 		}
 
+		// FIXME: Document this
+		void reset(T _x, T _y, T _width, T _height) {
+			x = _x;
+			y = _y;
+			width = _width;
+			height = _height;
+		}
+
+		// FIXME: Document this
+		void reset(Rect<T> rect) { reset(rect.x, rect.y, rect.width, rect.height); }
+
+		// FIXME: Document this
+		void move(T _x, T _y) { x += _x; y += _y; }
+
+		// FIXME: Document this
+		void move(Vector2<T> d) { move(d.x, d.y); }
+
+		// FIXME: Document this
 		Vector2<T> position() const { return {x, y}; }
 
+		// FIXME: Document this
 		void setPosition(Vector2<T> vector2) { x = vector2.x; y = vector2.y; }
 
+		// FIXME: Document this
 		Rect operator+(const Vector2<T> &vector2) const { return Rect{x + vector2.x, y + vector2.y, width, height}; }
+
+		// FIXME: Document this
 		Rect operator-(const Vector2<T> &vector2) const { return Rect{x - vector2.x, y - vector2.y, width, height}; }
 
+		// FIXME: Document this
 		Rect &operator+=(const Vector2<T> &vector2) { *this = operator+(vector2); return *this; }
+
+		// FIXME: Document this
 		Rect &operator-=(const Vector2<T> &vector2) { *this = operator-(vector2); return *this; }
 
+		////////////////////////////////////////////////////////////
+		/// \brief Overload of binary operator ==
+		///
+		/// This operator compares strict equality between two rectangles.
+		///
+		/// \param rect Rectangle to compare with
+		///
+		/// \return True if this rectangle is equal to \a rect
+		///
+		////////////////////////////////////////////////////////////
 		bool operator==(const Rect<T> &rect) const { return x == rect.x && y == rect.y && width == rect.width && height == rect.height; }
+
+		////////////////////////////////////////////////////////////
+		/// \brief Overload of binary operator !=
+		///
+		/// This operator compares strict difference between two rectangles.
+		///
+		/// \param rect Rectangle to compare with
+		///
+		/// \return True if this rectangle is not equal to \a rect
+		///
+		////////////////////////////////////////////////////////////
 		bool operator!=(const Rect<T> &rect) const { return !operator==(rect); }
 
-		T x = 0;
-		T y = 0;
-		T width = 0;
-		T height = 0;
+		////////////////////////////////////////////////////////////
+		// Member data
+		////////////////////////////////////////////////////////////
+		T x = 0;      ///< Left coordinate of the rectangle
+		T y = 0;      ///< Top coordinate of the rectangle
+		T width = 0;  ///< Width of the rectangle
+		T height = 0; ///< Height of the rectangle
 };
 
+// Create typedefs for the most common types
 using IntRect = Rect<int>;
 using FloatRect = Rect<float>;
 
 } // namespace gk
 
 #endif // GK_RECT_HPP_
+
+////////////////////////////////////////////////////////////
+/// \class sf::Rect
+/// \ingroup core
+///
+/// A rectangle is defined by its top-left corner and its size.
+/// It is a very simple class defined for convenience, so
+/// its member variables (left, top, width and height) are public
+/// and can be accessed directly, just like the vector classes
+/// (Vector2 and Vector3).
+///
+/// To keep things simple, gk::Rect doesn't define
+/// functions to emulate the properties that are not directly
+/// members (such as right, bottom, center, etc.), it rather
+/// only provides intersection functions.
+///
+/// gk::Rect uses the usual rules for its boundaries:
+/// \li The left and top edges are included in the rectangle's area
+/// \li The right (left + width) and bottom (top + height) edges are excluded from the rectangle's area
+///
+/// This means that gk::IntRect(0, 0, 1, 1) and gk::IntRect(1, 1, 1, 1)
+/// don't intersect.
+///
+/// gk::Rect is a template and may be used with any numeric type, but
+/// for simplicity the instantiations used by GameKit are typedef'd:
+/// \li gk::Rect<int> is gk::IntRect
+/// \li gk::Rect<float> is gk::FloatRect
+///
+/// So that you don't have to care about the template syntax.
+///
+/// Usage example:
+/// \code
+/// // Define a rectangle, located at (0, 0) with a size of 20x5
+/// gk::IntRect r1(0, 0, 20, 5);
+///
+/// // Define another rectangle, located at (4, 2) with a size of 18x10
+/// gk::Vector2i position(4, 2);
+/// gk::Vector2i size(18, 10);
+/// gk::IntRect r2(position, size);
+///
+/// // Test intersections with the point (3, 1)
+/// bool b1 = r1.contains(3, 1); // true
+/// bool b2 = r2.contains(3, 1); // false
+///
+/// // Test the intersection between r1 and r2
+/// gk::IntRect result;
+/// bool b3 = r1.intersects(r2, result); // true
+/// // result == (4, 2, 16, 3)
+/// \endcode
+///
+////////////////////////////////////////////////////////////
