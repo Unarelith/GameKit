3cdd6c7d33c0c89b9fe697f86a6ddbce9c9d9984
Quentin Bazin <quent42340@gmail.com>
2020-02-08 18:38:57 +0900

    Replaced my custom headers by LGPL license headers.

--- include/gk/scene/controller/EasyController.hpp
+++ include/gk/scene/controller/EasyController.hpp
@@ -1,13 +1,22 @@
 /*
  * =====================================================================================
  *
- *       Filename:  EasyController.hpp
+ *  GameKit
+ *  Copyright (C) 2018-2020 Unarelith, Quentin Bazin <openminer@unarelith.net>
  *
- *    Description:
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *        Created:  20/04/2019 15:26:02
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *         Author:  Quentin Bazin, <quent42340@gmail.com>
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  *
  * =====================================================================================
  */
