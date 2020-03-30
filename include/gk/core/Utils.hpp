ffc611fe82aa836ba5ab7a1bae802786e02e0e71
Quentin Bazin <quent42340@gmail.com>
2020-03-30 04:50:32 +0200

    [Utils] Added.

---
+++ include/gk/core/Utils.hpp
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *  GameKit
+ *
+ *  Copyright (C) 2018-2020 Unarelith, Quentin Bazin <openminer@unarelith.net>
+ *  Copyright (C) 2020 the GameKit contributors (see CONTRIBUTORS.md)
+ *
+ *  This file is part of GameKit.
+ *
+ *  GameKit is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  GameKit is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with GameKit; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * =====================================================================================
+ */
+#ifndef GK_UTILS_HPP_
+#define GK_UTILS_HPP_
+
+#include <sstream>
+
+namespace gk {
+
+template <typename T>
+std::string to_string(const T value, const int n = 6) {
+	std::ostringstream out;
+	out.precision(n);
+	out << std::fixed << value;
+	return out.str();
+}
+
+} // namespace gk
+
+#endif // GK_UTILS_HPP_
