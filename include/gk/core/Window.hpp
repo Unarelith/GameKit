bdb6d6c77638984ce6785f1f35f799ea23be0067
Quentin Bazin <quent42340@gmail.com>
2020-05-12 01:58:01 +0200

    [Window] Now supports resizing and pseudo-fullscreen mode.

--- include/gk/core/Window.hpp
+++ include/gk/core/Window.hpp
@@ -27,6 +27,7 @@
 #ifndef GK_WINDOW_HPP_
 #define GK_WINDOW_HPP_
 
+#include <functional>
 #include <string>
 
 #include <SFML/Window/Window.hpp>
@@ -36,19 +37,48 @@
 
 namespace gk {
 
-class Window : public RenderTarget, public sf::Window {
+class Window : public RenderTarget {
 	public:
 		void create(sf::VideoMode mode, const sf::String &title, sf::Uint32 style = sf::Style::Default, const sf::ContextSettings &settings = sf::ContextSettings());
 		void clear();
 
+		void onEvent(const sf::Event &event);
+
 		Vector2u getSize() const override { return m_size; }
+		void setSize(const Vector2u &size);
 
 		const View &getDefaultView() const override { return m_defaultView; }
 
+		bool isFullscreenModeEnabled() const { return m_isFullscreenModeEnabled; }
+		void setFullscreenMode(bool isFullscreenModeEnabled);
+
+		bool isVerticalSyncEnabled() const { return m_isVerticalSyncEnabled; }
+		void setVerticalSyncEnabled(bool isVerticalSyncEnabled);
+
+		void setOpenGLFlagsSetupFunc(const std::function<void(void)> &func) { m_glFlagsSetupFunc = func; }
+
+		sf::Window &window() { return m_window; }
+
 	private:
-		Vector2u m_size;
+		static void initOpenGL();
+
+		sf::Window m_window;
+
+		sf::VideoMode m_mode;
+		sf::String m_title;
+		sf::Uint32 m_style;
+		sf::ContextSettings m_settings;
+
+		Vector2u m_size{0, 0};
+		Vector2u m_baseSize{0, 0};
+		sf::Vector2i m_basePosition{0, 0};
 
 		View m_defaultView;
+
+		bool m_isFullscreenModeEnabled = false;
+		bool m_isVerticalSyncEnabled = false;
+
+		std::function<void(void)> m_glFlagsSetupFunc;
 };
 
 } // namespace gk
